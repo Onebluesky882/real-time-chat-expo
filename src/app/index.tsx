4070e211d94eab570d7f5d353959420e3c779060
Onebluesky882 <onebluesky882@outlook.com>
2026-02-16 09:50:52 +0700

    con : setup instantdb

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="">hello</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text>hello ter</Text>
     </View>
   );
 }
